b70bfe6abd2a1f62cc9c503dc52ff390af84a13f
tharon <tharonweighill@gmail.com>
2021-01-31 15:26:37 -0700

    commit tree af26ffe04bc178b93c79a50adcfcd13f58dc3b46

--- app.rb
+++ app.rb
@@ -1,16 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/reverse' do
-    erb :reverse
+  post '/reverse' do 
+    originalstring = params["string"] 
+    @reversedstring = original_string.reverse
+    erb :reversed
   end
 
-  post '/reverse' do
+  post '/reversed' do 
+    
     erb :reversed
   end
 
   get '/friends' do
-    # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+  
+    erb :friends
+  end 
 
-  end
+  
 end
